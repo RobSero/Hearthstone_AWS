98b8ac9566322d6917f89a48ef1bb6b87c8619df
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-09-27 09:38:15 +0100

    ac

--- src/lib/api.tsx
+++ src/lib/api.tsx
@@ -2,6 +2,7 @@ import axios from 'axios'
 // resets hourly
 const tempToken = 'US5PlklxisXxjE2u9DMaYs3fH44oL2o5uT'
 
+
 export async function getCardData(className:string) {
   return await axios.get(`https://us.api.blizzard.com/hearthstone/cards?locale=en_US&class=${className}&pageSize=200&access_token=${tempToken}`)
 }
